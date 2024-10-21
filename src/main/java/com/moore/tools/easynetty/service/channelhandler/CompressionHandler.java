75a1e75b776a695101c94175fbedee16fe13785e
mooreqv <mooreqv@gmail.com>
2024-10-21 19:07:53 +0800

    11

---
+++ src/main/java/com/moore/tools/easynetty/service/channelhandler/CompressionHandler.java
@@ -0,0 +1,11 @@
+package com.moore.tools.easynetty.service.channelhandler;
+
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * 压缩/解压处理器
+ * @author ：imoore
+ */
+public class CompressionHandler extends ChannelInboundHandlerAdapter {
+
+}
